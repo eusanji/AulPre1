1b57f167858a0be2399713a17ef4db1571dc05da
eusanji <eusanji@hotmail.com>
2018-06-23 14:22:55 -0300

    Camadas de MVC criadas

--- AulPre1/ConsoleApplication1/Program.cs
+++ AulPre1/ConsoleApplication1/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using Controllers;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleView
 {
     class Program
     {
         static void Main(string[] args)
         {
+          
+            //Criando objeto
+            AlunosController alunosController = new AlunosController();
+            
+            //Cadastrar alunos
+            Aluno a = CadastraAluno();
+            alunosController.Inserir(a);
+
+            Aluno b = CadastraAluno();
+            alunosController.Inserir(b);
+
+            Aluno c = CadastraAluno();
+            alunosController.Inserir(c);
+
+            foreach(Aluno aluno in alunosController.ListarTodos())
+            {
+                ImprimirDadosAluno(aluno);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimirDadosAluno(Aluno a)
+        {
+            Console.WriteLine("Aluno: " + a.Nome);//get
+            Console.WriteLine("Matricula: " + a.Matricula);//get
+        }
+
+        private static Aluno CadastraAluno()
+        {
+            Aluno a = new Aluno();
+            
+            Console.WriteLine("Digite o aluno");
+            a.Nome = Console.ReadLine();//set
+
+            Console.WriteLine("Digite a matricula");
+            a.Matricula = int.Parse(Console.ReadLine());//set
+            return a;
         }
     }
 }
---
+++ AulPre1/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+
+        public int Matricula { get; set; }
+
+        //private string Nome2 { get; set; }
+    }
+}
